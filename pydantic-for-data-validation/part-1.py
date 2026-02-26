9f83b9ee0b561b542bd7d2d2a8d86dc54b16ea06
sashreekpilla <pillasashreek>
2026-02-26 21:39:52 +0530

    imported validationError and printed  error message to incorrect inputs

--- pydantic-for-data-validation/part-1.py
+++ pydantic-for-data-validation/part-1.py
@@ -1,10 +1,14 @@
-from pydantic import BaseModel, Field, EmailStr, field_validator
+from pydantic import BaseModel, Field, EmailStr, field_validator, ValidationError
 
 class UserRegister(BaseModel):
     username: str = Field(min_length=5)
     email: EmailStr
     age: int = Field(ge=18)
 
-user_register_info = {'username' : 'Sashreek', 'email' : 'Sashreek@email.com', 'age' : 28}
-User = UserRegister(**user_register_info)
-print(User)
+try:
+    user_register_info = {'username' : 'Sashreek', 'email' : 'Sashreek@email.com', 'age' : 20}
+    User = UserRegister(**user_register_info)
+    print(User)
+
+except ValidationError as e:
+    print(e)
