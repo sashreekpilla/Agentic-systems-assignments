3721d56e7f9fc6ff17c3219b446a784d35bbbb15
sashreekpilla <pillasashreek>
2026-03-03 13:24:39 +0530

    added field validator to check pincode for digits

--- pydantic-hands-on/part-1.py
+++ pydantic-hands-on/part-1.py
@@ -14,11 +14,17 @@ class Address(BaseModel):
     city: str = Field(min_length=3)
     pincode: str = Field(min_length=6, max_length=6)
 
+    @field_validator("pincode")
+    def check_digits(cls, v):
+        if not v.isdigit():
+            raise ValueError("pincode must be digits")
+        return v
+
     model_config=ConfigDict(validate_assignment=True)
 
 
 try:
-    address= {'city':'hyd', 'pincode':'500050'}
+    address= {'city':'hyd', 'pincode':'abcdef'}
     user = {'user_id':'45','name' : 'Sashreek', 'email' : 'sashreek@gmail.com', 'age' : 20, 'address':address, 'is_premium':'0'}
     User1 = User(**user)
     print(User1)
