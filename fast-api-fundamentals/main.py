936db242bc985d941d1b40eaee81573fc9c6a274
sashreekpilla <pillasashreek>
2026-03-03 16:20:13 +0530

    Deleted customer.json and added 2 optional parameters (product and reiview). Returning the data entered in the query parameters.

--- fast-api-fundamentals/main.py
+++ fast-api-fundamentals/main.py
@@ -4,27 +4,11 @@ import json
 app = FastAPI()
 
 
-#load customers data from customer.json file
-def load_cusotmers_data():
-    with open('customer.json','r') as f:
-        customers_data = json.load(f)
-
-    return customers_data
-
-
-
 #Query parameter
+#product and review are optional parameters
 @app.get("/search")
-def search_via_name_age(name: str=Query(default='Deepak',description="name to be searched by"), age:int=Query(default=30,description="age to be searched by")):
-
-    data=load_cusotmers_data()
-
-    if name not in data:
-        raise HTTPException(status_code=404, detail="Details with given name or age are not found name")
-    
-    if age not in data:
-        raise HTTPException(status_code=404, detail="Details with given name or age are not found age")
-    
-    
+def search_via_name_age(name: str, age: int, product: str |None=None, Review: str |None=None):
+    data={"name": name, "age": age}
+    #data returning as json
     return data
 
