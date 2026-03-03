54186a7a32e5b8afe04fb15d0f0247506d8d7ca2
sashreekpilla <pillasashreek>
2026-03-03 17:04:59 +0530

    validation for name to have only characters

--- fast-api-fundamentals/main.py
+++ fast-api-fundamentals/main.py
@@ -7,8 +7,14 @@ app = FastAPI()
 #Query parameter
 #product and review are optional parameters
 @app.get("/search")
-def search_via_name_age(name: str, age: int, product: str |None=None, Review: str |None=None):
+def search_via_name_age(name: str=Query(..., description="Name of the customer"), age: int=Query(..., description="age of the customer"), product: str |None=None, Review: str|None=None):
+
+
+    # validation of name to have only characters
+    if any(char.isdigit() for char in name):
+        raise HTTPException(status_code=404,detail="name should not have digits")
+
     data={"name": name, "age": age}
-    #data returning as json
+    #returning data as json
     return data
 
