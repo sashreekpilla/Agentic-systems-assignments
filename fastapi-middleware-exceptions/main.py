97b2e20834e0c7035d8b38f4fd4730b780236953
sashreekpilla <pillasashreek>
2026-03-08 13:56:56 +0530

    middleware and exception handling (404 error) in fast api

---
+++ fastapi-middleware-exceptions/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, Request, HTTPException, status
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+
+#simple API endpoint to return JSON
+@app.get("/hello")
+def hello():
+    return {"message" : "Hello, Welcome to FastAPI!"}
+
+
+#middleware function
+@app.middleware("http")
+async def first_middleware(request: Request, call_next):
+
+    print("Message before the request is processed")
+
+    response = await call_next(request)
+
+    print("Message after the request is processed")
+
+    return response
+
+
+#custom 404 exception handler for undefined routes
+@app.exception_handler(status.HTTP_404_NOT_FOUND)
+async def custom_404_handler(request: Request, exc: HTTPException):
+
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={
+            "message": "The requested resource was not found."
+        },
+    )
